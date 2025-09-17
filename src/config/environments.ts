97912057f6ad7b1b60ebcfef3e5f61bbe06f0f1a
alejandro-montepeque <jonathanmontepeque@gmail.com>
2025-09-17 15:40:08 -0600

    fix:deployment

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,8 +1,7 @@
-import 'dotenv/config';
 import * as env from 'env-var';
 
 export const environments = {
-    PORT: env.get('PORT').required().asPortNumber(),
+    PORT: env.get('PORT').default('3000').asPortNumber(),
     MONGODB_URI: env.get('MONGODB_URI').required().asString(),
-    MONGODB_DBNAME: env.get('MONGODB_DBNAME').required().asString(),
+    MONGODB_DBNAME: env.get('MONGODB_DBNAME').default('sample_mflix').asString(),
 };
